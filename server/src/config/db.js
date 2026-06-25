dd4c2e0234a120995a1ca4de288f841ecd450f89
HaroldMars <abejar199@gmail.com>
2026-06-25 10:49:14 +0800

    Update mongoDb

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -6,7 +6,14 @@ export const connectDB = async () => {
     if (!uri) {
       throw new Error("MONGO_URI is not defined in your .env file");
     }
-    const conn = await mongoose.connect(uri);
+    const conn = await mongoose.connect(uri, {
+        serverApi: {
+            version: "1",
+            strict: false,
+            deprecationErrors: true
+        },
+    });
+    
     console.log(`[db] MongoDB connected: ${conn.connection.host}`);
   } catch (err) {
     console.error(`[db] Connection error: ${err.message}`);
